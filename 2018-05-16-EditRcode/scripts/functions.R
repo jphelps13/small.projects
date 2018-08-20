846f60b59020c90da8bd452815a23a6fad7429b0
Jonathan Phelps <jphelps130591@gmail.com>
2018-08-20 10:20:44 +0100

    post on editing R codes

---
+++ 2018-05-16-EditRcode/scripts/functions.R
@@ -0,0 +1,5 @@
+# sample file with functions in
+fun_clean <- function(x){x}
+fun_visualise_data <- function(x){x}
+Fun.Model <- function(x){x}
+funReport <- function(x){x}
---
+++ 2018-05-16-EditRcode/scripts/run.R
@@ -0,0 +1,6 @@
+# sample script
+x <- 1
+fun_clean(x)
+fun_visualise_data(x)
+Fun.Model(x)
+funReport(x)
